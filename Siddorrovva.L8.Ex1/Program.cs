690a646406516aeb66ca448fbfb35c799dbd46ef
sidnatasha03 <90253059+sidnatasha03@users.noreply.github.com>
2022-05-22 16:39:19 +0300

    Add files via upload

---
+++ Siddorrovva.L8.Ex1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+string file1 = @"Text.txt";
+string path = @"C:\Users\пёс\source\repos\Sidorova.Lab8\Sidorova.Lab8\bin\Debug\net6.0\Text.txt";
+DirectoryInfo dirInfo = new DirectoryInfo(path);
+FileInfo info = new FileInfo(file1);
+Console.WriteLine($"Абсолютный путь: {info.FullName}");
+Console.WriteLine($"Относительный путь: {info.Name}");
+Console.WriteLine($"Дата: {info.CreationTime}");
+Console.WriteLine($"Размер:{info.Length} байт");
+
+Console.WriteLine();
+
+
+Console.WriteLine(Path.GetFileNameWithoutExtension(file1));
+Console.WriteLine(Environment.CurrentDirectory);
+
+Console.WriteLine();
+
+if (File.Exists(file1))
+{
+    string text = File.ReadAllText(file1);
+
+
+    int i = 0;
+    var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    for (i = 0; i < words.Length; i++)
+    {
+        i++;
+
+
+
+    }
+
+    Console.WriteLine($"Всего слов:{i.ToString()}");
+
+    bool Flag1 = false;
+    bool Flag2 = false;
+    bool Flag3 = false;
+
+    for (int k = 0; k < text.Length; k++)
+    {
+
+        if (text[k] >= 'a' && text[k] <= 'z' || text[k] >= 'A' && text[k] <= 'Z')
+        {
+            Flag1 = true;
+
+
+        }
+        if (text[k] >= 'а' && text[k] <= 'я' || text[k] >= 'А' && text[k] <= 'Я')
+        {
+            Flag2 = true;
+
+
+        }
+        if (Char.IsDigit(text[k]))
+        {
+            Flag3 = true;
+
+        }
+
+
+    }
+    if (Flag3)
+    {
+        Console.WriteLine("В тексте есть цифры");
+    }
+    if (Flag2)
+    {
+        Console.WriteLine("В тексте есть кириллица");
+    }
+    if (Flag1)
+    {
+        Console.WriteLine("В тексте есть латиница");
+    }
+
+    var stat = words.Distinct()
+            .ToDictionary(word => word, word => words.Count(x => x == word))
+            .OrderByDescending(x => x.Value);
+    Console.WriteLine();
+
+    StringBuilder builder = new StringBuilder();
+    foreach (var item in stat)
+    {
+        Console.WriteLine(item);
+        builder.AppendLine($"{item.Key} {item.Value}");
+
+    }
+
+    StreamReader reader = new StreamReader("Text.txt");
+
+    int j = 0;
+    while ((text = reader.ReadLine()) != null)
+    {
+        j++;
+    }
+    Console.WriteLine("Всего строк:" + j.ToString());
+
+
+    builder.AppendLine($"{info.FullName}\n{info.Name}\n{info.CreationTime}\n{info.Length} байт\nВсего строк: {j.ToString()}\nВсего слов: {i.ToString()}");
+
+    File.WriteAllText("result1.txt", builder.ToString());
+
+}
+else
+{
+    Console.WriteLine("Такого файла не существует!");
+}
+
---
+++ Siddorrovva.L8.Ex2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Text.Encodings;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+{
+
+    var path = "Table.csv";
+
+    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+    Encoding encoding = Encoding.GetEncoding(1251);
+
+    var lines = File.ReadAllLines(path, encoding);
+    Console.WriteLine(lines);
+    var bakery = new Bakery[lines.Length - 1];
+
+    for (var i = 1; i < lines.Length; i++)
+    {
+        var splits = lines[i].Split(';');
+        var bake = new Bakery();
+        bake.Название_пекарни = splits[0];
+        bake.Общая_производительность_продукции = Convert.ToDouble(splits[1]);
+        bake.Средняя_стоимость_продукции = Convert.ToDouble(splits[2]);
+        bake.Средние_издержки_в_мес = Convert.ToDouble(splits[3]);
+        bake.Среднее_количество_покупателей_в_день = Convert.ToDouble(splits[4]);
+        bakery[i - 1] = bake;
+    }
+
+    var result = "result.csv";
+    using (StreamWriter streamWriter = new StreamWriter(result, false, encoding))
+    {
+        streamWriter.WriteLine($"Название;Производительность;Стоимость;Издержки;Покупатели;Доход;Прибыль");
+        for (int i = 0; i < bakery.Length; i++)
+        {
+            streamWriter.WriteLine(bakery[i].ToExcel());
+        }
+    }
+
+    var jsonOptions = new JsonSerializerOptions()
+    {
+        Encoder = JavaScriptEncoder.Default
+    };
+
+    var json = JsonSerializer.Serialize(bakery, jsonOptions);
+    File.WriteAllText("result.json", json);
+
+    var stringJson = File.ReadAllText("result.json");
+    var array = JsonSerializer.Deserialize<Bakery[]>(stringJson);
+    foreach (var item in array)
+    {
+        Console.WriteLine(item.ToString());
+    }
+
+    string jsonNewtonsoft = Newtonsoft.Json.JsonConvert.SerializeObject(bakery, Newtonsoft.Json.Formatting.Indented);
+
+
+    File.WriteAllText("NewtonsoftResult.json", jsonNewtonsoft);
+}
+
+
+
+
+public class Bakery
+{
+    public string Название_пекарни { get; set; }
+    public double Общая_производительность_продукции { get; set; }
+    public double Средняя_стоимость_продукции { get; set; }
+
+    public double Средние_издержки_в_мес { get; set; }
+
+    public double Среднее_количество_покупателей_в_день { get; set; }
+
+    public double Средний_доход_в_месяц { get => Среднее_количество_покупателей_в_день * 12 * Средняя_стоимость_продукции; }
+    public double Средняя_прибыль_в_месяц { get => Средний_доход_в_месяц - Средние_издержки_в_мес; }
+    public override string ToString()
+    {
+        return $" {Название_пекарни}\n Общая производительность: {Общая_производительность_продукции} изделий в день\n Средняя стоимость продукции: {Средняя_стоимость_продукции}\n Средние издержки: {Средние_издержки_в_мес} в мес\n Среднее количество покупателей:{Среднее_количество_покупателей_в_день} в день\n Средний доход в месяц:{Средний_доход_в_месяц} рублей в месяц\n Средняя прибыль в месяц: {Средняя_прибыль_в_месяц} рублей в месяц";
+    }
+
+    public string ToExcel()
+    {
+        return $" {Название_пекарни};{Общая_производительность_продукции};{Средняя_стоимость_продукции};{Средние_издержки_в_мес};{Среднее_количество_покупателей_в_день};{Средний_доход_в_месяц}; {Средняя_прибыль_в_месяц}";
+
+    }
+}
+
